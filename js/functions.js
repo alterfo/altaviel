840484d28fc63144acba13c5b129eaf96a7c8c03
Олег Сидоркин <olegsidorkin@NameOfComputer.local>
2013-11-27 00:51:40 +0400

    init

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+(function detectBrowser() {
+  var useragent = navigator.userAgent;
+  var mapdiv = document.getElementById("map-canvas");
+
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '100%';
+  } else {
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '600px';
+  }
+})();
